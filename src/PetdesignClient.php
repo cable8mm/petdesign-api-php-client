49a8af489c4eed041cb7543d8eae88494dd05997
Sam Lee <cable8mm@gmail.com>
2020-05-17 15:04:15 +0900

    Add namespace and psr-12

--- src/PetdesignClient.php
+++ src/PetdesignClient.php
@@ -1,55 +1,63 @@
 <?php
-class PetdesignClient {
-    const LIBVER = "1.0.0";
-    const API_BASE_PATH = "http://api.petsdesign.co.kr";
-    const TEST_API_KEY = "cable8mm";
+
+namespace Cable8mm\PetdesignClient;
+
+class PetdesignClient
+{
+    const LIBVER = '1.0.0';
+    const API_BASE_PATH = 'http://api.petsdesign.co.kr';
+    const TEST_API_KEY = 'cable8mm';
 
     private $config;
 
-    public function __construct(array $config = array()) {
+    public function __construct(array $config = [])
+    {
         $this->config = array_merge(
-        [
-          'base_path' => self::API_BASE_PATH,
-          'api_key' => self::TEST_API_KEY
-        ],
-        $config
+            [
+                'base_path' => self::API_BASE_PATH,
+                'api_key' => self::TEST_API_KEY
+            ],
+            $config
         );
 
         return;
     }
 
-    public function getGoods($params = []) {
-        switch($params['type']) {
+    public function getGoods($params = [])
+    {
+        switch ($params['type']) {
             case 'code':
                 $opts = [
-                    "http" => [
-                        "method" => "GET",
-                        "header" => "cache-control: no-cache\r\n" .
-                            "pd_key: ".$this->config['api_key']."\r\n"
+                    'http' => [
+                        'method' => 'GET',
+                        'header' => "cache-control: no-cache\r\n" .
+                            'pd_key: ' . $this->config['api_key'] . "\r\n"
                     ]
                 ];
                 $context = stream_context_create($opts);
-                return file_get_contents(self::API_BASE_PATH.'/goods/'.$params['code'], false, $context);
+                return file_get_contents(self::API_BASE_PATH . '/goods/' . $params['code'], false, $context);
             break;
             case 'all':
                 $opts = [
-                    "http" => [
-                        "method" => "GET",
-                        "header" => "cache-control: no-cache\r\n" .
-                            "pd_key: ".$this->config['api_key']."\r\n"
+                    'http' => [
+                        'method' => 'GET',
+                        'header' => "cache-control: no-cache\r\n" .
+                            'pd_key: ' . $this->config['api_key'] . "\r\n"
                     ]
                 ];
                 $context = stream_context_create($opts);
-                return file_get_contents(self::API_BASE_PATH.'/goods/all/'.$params['page'], false, $context);
+                return file_get_contents(self::API_BASE_PATH . '/goods/all/' . $params['page'], false, $context);
             break;
         }
     }
 
-    public function getGoodsByCode($code) {
-        return $this->getGoods(['type'=>'code', 'code'=>$code]);
+    public function getGoodsByCode($code)
+    {
+        return $this->getGoods(['type' => 'code', 'code' => $code]);
     }
 
-    public function getAllGoods($page = 1) {
-        return $this->getGoods(['type'=>'all', 'page'=>$page]);
+    public function getAllGoods($page = 1)
+    {
+        return $this->getGoods(['type' => 'all', 'page' => $page]);
     }
 }
