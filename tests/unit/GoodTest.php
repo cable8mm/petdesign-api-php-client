2f6856012e39842fb16fe0e125d519ec8add483a
Sam Lee <cable8mm@gmail.com>
2020-05-27 20:22:37 +0900

    Added test code

---
+++ tests/unit/GoodTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EscCompany\PetdesignApiClient\Tests\Functional;
+
+use EscCompany\PetdesignApiClient\API\Good;
+use Dotenv\Dotenv;
+use PHPUnit\Framework\TestCase;
+
+class GoodTest extends TestCase
+{
+    protected $apiKey;
+
+    protected function setUp(): void
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        $this->apiKey = getenv('JUNGLE_BOOK_API_KEY');
+    }
+
+    public function test_get_config()
+    {
+        $this->assertNotNull(getenv('JUNGLE_BOOK_API_KEY'));
+    }
+
+    public function test_get_by_id()
+    {
+        $good = (new Good($this->apiKey))->find(3754);
+
+        $this->assertIsArray($good);
+    }
+
+    public function test_exist_keys_by_find()
+    {
+        $good = (new Good($this->apiKey))->find(3754);
+
+        $this->assertArrayHasKey('goodsNo', $good);
+        $this->assertArrayHasKey('open', $good);
+        $this->assertArrayHasKey('goodsNm', $good);
+        $this->assertArrayHasKey('categoryNm', $good);
+    }
+}
