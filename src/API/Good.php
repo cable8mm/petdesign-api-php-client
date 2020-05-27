96ae215d64f8ca95938d28817762d1b76c600074
Sam Lee <cable8mm@gmail.com>
2020-05-27 15:34:41 +0900

    Added look-up goods API and base class

---
+++ src/API/Good.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Cable8mm\PetdesignApiPhpClient\API;
+
+use Cable8mm\PetdesignApiPhpClient\Contracts\Request;
+use InvalidArgumentException;
+
+class Good extends Request
+{
+    private $page = 1;
+    private $visible = true;
+    private $id;
+    private $from;  // ?updateAfter=2019-09-01
+    private $tag;
+
+    private $allowedTags = [
+        '긴급소진', // 유통기한 임박 또는 악성재고 상품으로 분류되어 빠르게 소진 해야 할 상품
+        '본사품절', // 유통본사 재고가 없는 상품으로 분류되어 발주 및 입고예정일을 확인 할 수 없는 상품
+        '롱-리드',  // 재입고까지 최소 2주 이상 걸리는 리드타임이 긴 상품
+        '단종',     // 단종된 상품
+        '오프전용', // 오프라인에서만 판매/유통 할 수 있는 상품
+        '단가인상', // 단가인상 예정인 상품
+    ];
+
+    private function builder()
+    {
+        $this->query = 'goods/';
+
+        if (!empty($this->id)) {
+            $this->query .= $this->id;
+            return $this;
+        }
+
+        // 태그로 조회
+        if (!empty($this->tag)) {
+            $this->query .= '!' . $this->tag;
+        } else {
+            // 전체상품 조회
+            if ($this->visible === true) {
+                $this->query .= 'all';
+            }
+
+            // 미진열상품 조회
+            if ($this->visible === false) {
+                $this->query .= 'close';
+            }
+        }
+
+        // 조건 파라미터
+        if (!empty($this->from)) {
+            $this->query .= '?updateAfter=' . $this->from;
+        }
+
+        // 페이지
+        if (empty($this->from)) {
+            $this->query .= '/' . $this->page;
+        }
+
+        return $this;
+    }
+
+    public function find(int $id = null)
+    {
+        $this->id = $id;
+
+        // 특정상품 조회
+        if (!is_null($id)) {
+            return $this->builder()->get('data', 0);
+        }
+
+        return $this;
+    }
+
+    public function page(int $page)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    public function show(bool $tf)
+    {
+        $this->visible = $tf;
+
+        return $this;
+    }
+
+    public function tag(string $tag)
+    {
+        if (!in_array($tag, $this->allowedTags)) {
+            throw new InvalidArgumentException(__METHOD__);
+        }
+
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * @param string $date Y-m-d
+     * @return $this
+     */
+    public function from(string $date)
+    {
+        $this->from = $date;
+
+        return $this;
+    }
+}
---
+++ src/Contracts/Request.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Cable8mm\PetdesignApiPhpClient\Contracts;
+
+use GuzzleHttp\Client;
+
+abstract class Request
+{
+    const API_BASE_PATH = 'http://api.petsdesign.co.kr/';
+
+    /**
+     * API Key
+     *
+     * @var string
+     * @see https://github.com/junglebookDevTeam/API or dev@junglebook.co.kr
+     */
+    protected static $apiKey;
+
+    protected $query = '';
+
+    /**
+     * return response from API Server
+     *
+     * @var GuzzleHttp\Psr7\Response
+     */
+    protected $response;
+
+    /**
+     * @var GuzzleHttp\Client
+     */
+    protected $client;
+
+    public function __construct(string $apiKey)
+    {
+        self::$apiKey = $apiKey;
+
+        $config = [
+            'base_uri' => self::API_BASE_PATH,
+            'headers' => [
+                'cache-control' => 'no-cache',
+                'Authorization' => self::$apiKey,
+            ]
+        ];
+
+        $this->client = new Client($config);
+    }
+
+    public function get($key = null, $offset = null)
+    {
+        $this->response = $this->client->get($this->query);
+
+        $contents = json_decode($this->response->getBody()->getContents(), true);
+
+        if (!is_null($key) && !is_null($offset)) {
+            return $contents[$key][$offset];
+        }
+
+        return $contents;
+    }
+}
